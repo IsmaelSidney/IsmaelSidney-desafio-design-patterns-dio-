6e4a586e46d3b8f6bb0d590f9c4dc4a69ec166db
Ismael Barroso <mael-sf@hotmail.com>
2022-05-29 17:58:37 -0300

    add DP-singleton

---
+++ src/dio/designpatterns/SingletonLazyHolder.java
@@ -0,0 +1,18 @@
+package dio.designpatterns;
+
+public class SingletonLazyHolder {
+
+    private static class InstaceHolder{
+        public static SingletonLazyHolder instancia =new SingletonLazyHolder();
+    }
+
+
+    private SingletonLazyHolder(){
+        super();
+    }
+
+    public static SingletonLazyHolder getInstancia(){
+
+        return InstaceHolder.instancia;
+    }
+}
