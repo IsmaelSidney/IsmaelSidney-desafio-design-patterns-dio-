ec504a2d88ef61e9753688186d06412be59dbf20
Ismael Barroso <mael-sf@hotmail.com>
2022-05-29 18:30:39 -0300

    add DP strategy

--- src/dio/designpatterns/Teste.java
+++ src/dio/designpatterns/Teste.java
@@ -3,26 +3,42 @@ package dio.designpatterns;
 import dio.designpatterns.singleton.SingletonEager;
 import dio.designpatterns.singleton.SingletonLazy;
 import dio.designpatterns.singleton.SingletonLazyHolder;
+import dio.designpatterns.strategy.*;
 
 public class Teste {
 
     public static void main(String[] args) {
 
-        //Testes para Design patterns Singleton
+        //Testes para Design patterns
+        // Singleton
 
-        SingletonLazy lazy = SingletonLazy.getInstancia();
-        System.out.println(lazy);
-        lazy = SingletonLazy.getInstancia();
-        System.out.println(lazy);
+//        SingletonLazy lazy = SingletonLazy.getInstancia();
+//        System.out.println(lazy);
+//        lazy = SingletonLazy.getInstancia();
+//        System.out.println(lazy);
+//
+//        SingletonEager eager = SingletonEager.getInstancia();
+//        System.out.println(eager);
+//        eager = SingletonEager.getInstancia();
+//        System.out.println(eager);
+//
+//        SingletonLazyHolder holder = SingletonLazyHolder.getInstancia();
+//        System.out.println(holder);
+//        holder = SingletonLazyHolder.getInstancia();
+//        System.out.println(holder);
 
-        SingletonEager eager = SingletonEager.getInstancia();
-        System.out.println(eager);
-        eager = SingletonEager.getInstancia();
-        System.out.println(eager);
+        //Strategy
 
-        SingletonLazyHolder holder = SingletonLazyHolder.getInstancia();
-        System.out.println(holder);
-        holder = SingletonLazyHolder.getInstancia();
-        System.out.println(holder);
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
     }
 }
---
+++ src/dio/designpatterns/strategy/Comportamento.java
@@ -0,0 +1,5 @@
+package dio.designpatterns.strategy;
+
+public interface Comportamento {
+    void mover();
+}
